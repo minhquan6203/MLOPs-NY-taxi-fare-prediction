a5d96ff41ef6f0ebe2bed6cc896f28a97b4b50a2
minhquan6203 <harryquan.minh@gmail.com>
2024-06-07 21:20:37 +0700

    first

--- deployment/score.py
+++ deployment/score.py
@@ -11,10 +11,10 @@ from azureml.core.model import Model
 
 
 def init():
-    global LGBM_MODEL
+    global model
     # Load the model from file into a global object
     model_path = Model.get_model_path("nytaxifareamount_model")
-    LGBM_MODEL = joblib.load(model_path)
+    model = joblib.load(model_path)
     # Print statement for appinsights custom traces:
     print("model initialized" + time.strftime("%H:%M:%S"))
 
@@ -27,7 +27,7 @@ def init():
 def run(raw_data, request_headers):
     data = json.loads(raw_data)["data"]
     data = numpy.array(data)
-    result = LGBM_MODEL.predict(data)
+    result = model.predict(data)
     # Log the input and output data to appinsights:
     info = {
         "input": raw_data,
